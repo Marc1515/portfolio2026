b880f73a92a823d5cc672359132a7e6546bf5645
Marc1515 <meq.1515@gmail.com>
2026-06-08 16:11:51 +0100

    Refactor ProjectCard component to adjust description height
    - Removed unnecessary flex property from the article element.
    - Increased minimum height of the description paragraph for better content visibility.

--- src/components/features/projects/ProjectCard.tsx
+++ src/components/features/projects/ProjectCard.tsx
@@ -13,7 +13,7 @@ export function ProjectCard({ project }: ProjectCardProps) {
   const description = t(`items.${project.translationKey}.description`);
 
   return (
-    <article className="flex flex-col justify-between min-h-[440px] bg-(--surface) border border-(--surface-border) rounded-(--radius) p-(--space-2)!">
+    <article className="flex flex-col min-h-[440px] bg-(--surface) border border-(--surface-border) rounded-(--radius) p-(--space-2)!">
       <div className="aspect-video overflow-hidden rounded-lg border border-(--surface-border)">
         <Image
           src={project.image}
@@ -28,7 +28,7 @@ export function ProjectCard({ project }: ProjectCardProps) {
       </div>
       <div className="flex flex-col gap-2 pt-4!">
         <h3>{title}</h3>
-        <p className="min-h-20">{description}</p>
+        <p className="min-h-28">{description}</p>
         <ul className="flex gap-2" aria-label={t("techStackAria", { title })}>
           {project.tags.map((tag) => (
             <li
